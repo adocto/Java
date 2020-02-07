8b401ea87be12494e46c0bcded018300c37da17e
Andrew Doctor <adocto@users.noreply.github.com>
2020-02-07 17:41:52 -0500

    Update Store

--- codecademy/src/Store/Store.java
+++ codecademy/src/Store/Store.java
@@ -1,19 +1,19 @@
 public class Store {
-    // declare instance fields here!
-
+    // instance fields
+    String productType;
+    int inventoryCount;
+    double inventoryPrice;
 
     // constructor method
-    public Store() {
-        System.out.println("I am inside the constructor method.");
-        String productType;
+    public Store(String product, int count, double price) {
+        productType = product;
+        inventoryCount = count;
+        inventoryPrice = price;
     }
 
     // main method
     public static void main(String[] args) {
-        System.out.println("This code is inside the main method.");
-
-        Store lemonadeStand = new Store();
-
-        System.out.println(lemonadeStand);
+        Store cookieShop = new Store("cookies",12,3.75);
+        System.out.println(cookieShop.productType);
     }
 }
