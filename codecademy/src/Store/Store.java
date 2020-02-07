72c7055df9241575d78e11749f652f87c5940c2e
Andrew Doctor <adocto@users.noreply.github.com>
2020-02-07 17:32:01 -0500

    Create Store.java

---
+++ codecademy/src/Store/Store.java
@@ -0,0 +1,24 @@
+public class Store {
+
+
+    // new method: constructor!
+    public Store() {
+        System.out.println("I am inside the constructor method.");
+    }
+
+    // main method is where we create instances!
+    public static void main(String[] args) {
+        System.out.println("Start of the main method.");
+
+        Store lemonadeStand = new Store();
+
+        System.out.println(lemonadeStand);
+
+
+
+        // create the instance below
+
+        // print the instance below
+
+    }
+}
