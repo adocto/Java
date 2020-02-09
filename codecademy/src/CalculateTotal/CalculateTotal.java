0ec29cd53efbeb6e33dcb9854cc6180aa2b1cee5
Andrew Doctor <adocto@users.noreply.github.com>
2020-02-09 17:06:26 -0500

    Update CalculateTotal.java

--- codecademy/src/CalculateTotal/CalculateTotal.java
+++ codecademy/src/CalculateTotal/CalculateTotal.java
@@ -13,7 +13,9 @@ class CalculateTotal {
         double total = 0;
 
         // Iterate over expenses
-
+        for(int i = 0; i < expenses.size(); i++){
+            total = total + expenses.get(i);
+        }
 
         System.out.println(total);
 
