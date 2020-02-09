bbebf0ae86cc37915229024905a240eab87da9c0
Andrew Doctor <adocto@users.noreply.github.com>
2020-02-09 16:54:07 -0500

    Create LuckyFive

--- codecademy/src/ToDos/ToDos.java
+++ codecademy/src/ToDos/ToDos.java
@@ -10,27 +10,18 @@ class ToDos {
         sherlocksToDos.add("visit the crime scene");
         sherlocksToDos.add("play violin");
         sherlocksToDos.add("interview suspects");
+        sherlocksToDos.add("listen to Dr. Watson for amusement");
         sherlocksToDos.add("solve the case");
         sherlocksToDos.add("apprehend the criminal");
 
-        // Poirot
-        ArrayList<String> poirotsToDos = new ArrayList<String>();
-
-        poirotsToDos.add("visit the crime scene");
-        poirotsToDos.add("interview suspects");
-        poirotsToDos.add("let the little grey cells do their work");
-        poirotsToDos.add("trim mustache");
-        poirotsToDos.add("call all suspects together");
-        poirotsToDos.add("reveal the truth of the crime");
-
-        // Remove each to-do below:
         sherlocksToDos.remove("visit the crime scene");
-        poirotsToDos.remove("visit the crime scene");
-        sherlocksToDos.remove("play violin");
 
+        // Calculate to-dos until case is solved:
+        int howlong = sherlocksToDos.indexOf("solve the case");
 
-        System.out.println(sherlocksToDos.toString() + "\n");
-        System.out.println(poirotsToDos.toString());
+        // Change the value printed:
+        System.out.println(howlong);
+
     }
 
 }
