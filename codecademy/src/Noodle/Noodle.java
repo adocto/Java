0052413a5520c0ce66e63a7a6077f5eba5c4e955
Andrew Doctor <adocto@users.noreply.github.com>
2020-02-10 23:20:57 -0500

    Update noodle

--- codecademy/src/Noodle/Noodle.java
+++ codecademy/src/Noodle/Noodle.java
@@ -17,10 +17,24 @@ class Noodle {
 
     }
 
-    public void cook() {
+    public String getCookPrep() {
 
-        this.texture = "cooked";
+        return "Boil noodle for 7 minutes and add sauce.";
 
+    }
+
+
+    public static void main(String[] args) {
+
+        Noodle spaghetti, ramen, pho;
+
+        spaghetti = new Spaghetti();
+        ramen = new Ramen();
+        pho = new Pho();
+
+        // Add your code below:
+        Noodle[] allTheNoodles = {spaghetti, ramen,pho};
+        
     }
 
 }
