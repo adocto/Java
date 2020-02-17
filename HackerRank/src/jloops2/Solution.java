bcd8ac765f04b5034332bb249ecb1df9af8c1119
Andrew Doctor <adocto@users.noreply.github.com>
2020-02-16 21:52:49 -0500

    Create jdatatype

--- HackerRank/src/jloops2/Solution.java
+++ HackerRank/src/jloops2/Solution.java
@@ -5,10 +5,24 @@ class Solution{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int t=in.nextInt();
+        int num1 = 0;
+        int exp1;
         for(int i=0;i<t;i++){
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
+            num1 = a;
+            for(double j = 0; j<n; j++){
+                exp1 = (int) Math.pow(2,j);
+                num1 = num1 + (exp1 * b);
+                if(j  == 0){
+                    System.out.print(num1);
+                }else{
+                    System.out.printf(" %d",num1);
+                }
+            }
+            System.out.println();
+            num1 = 0;
         }
         in.close();
     }
