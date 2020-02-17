7b13ea8008e7607ed1977f589bcee86853ba0537
Andrew Doctor <adocto@users.noreply.github.com>
2020-02-16 22:05:18 -0500

    Update Solution.java

--- HackerRank/src/jdatatype/Solution.java
+++ HackerRank/src/jdatatype/Solution.java
@@ -8,9 +8,10 @@ class Solution{
     {
 
 
-
         Scanner sc = new Scanner(System.in);
         int t=sc.nextInt();
+        long lLim = (long) Math.pow(-2,63);
+        long uLim = (long) Math.pow(2,63) -1;
 
         for(int i=0;i<t;i++)
         {
@@ -19,8 +20,25 @@ class Solution{
             {
                 long x=sc.nextLong();
                 System.out.println(x+" can be fitted in:");
-                if(x>=-128 && x<=127)System.out.println("* byte");
+                switch (x) {
+                    case((x >= lLim) && (x <= uLim)):
+                    case((x >= -2147483648)&&(x <=  2147483647)):
+                        System.out.println("* byte");
+                        System.out.println("* short");
+                        System.out.println("* int");
+                        break;
+                    case ((x >= -32768) && (x <= 32767 )):
+                        System.out.println("* byte");
+                        System.out.println("* short");
+                        break;
+                    case ((x >= -128) && (x <= 127)):
+                        System.out.println("* byte");
+                        break;
+                }
                 //Complete the code
+
+
+
             }
             catch(Exception e)
             {
