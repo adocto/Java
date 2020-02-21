696225afef8211cd6ad3dcf82804a8b562a44069
Andrew Doctor <adocto@users.noreply.github.com>
2020-02-21 14:41:56 -0500

    Finish end of file

---
+++ HackerRank/src/end_of_file/Solution.java
@@ -0,0 +1,21 @@
+package end_of_file;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = 0;
+        String s;
+        while(scan.hasNext()){
+            i ++;
+            s = scan.nextLine();
+            System.out.printf("%d %s%n", i, s);
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
