3022f2a21420d1fffd6eabc91d342d71e110a413
Andrew Doctor <adocto@users.noreply.github.com>
2020-02-29 15:30:30 -0500

    Update Solution.java

--- HackerRank0/src/substringComparisonsForJava/Solution.java
+++ HackerRank0/src/substringComparisonsForJava/Solution.java
@@ -5,11 +5,20 @@ public class Solution {
     public static String getSmallestAndLargest(String s, int k) {
         String smallest = "";
         String largest = "";
+        int length = s.length();
         int left =0;
         int right = k-1;
-        String currentString;
+        String currentString = s.substring(left,right);
+        int i;
+        smallest = currentString;
+        largest = currentString;
 
-        
+        for(i = 0, i < length, i++){
+            currentString = s.substring(left,right);
+            
+        }
+
+
 
 
         // Complete the function
