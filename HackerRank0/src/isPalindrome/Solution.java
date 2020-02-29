40d9ae35a3a5bb921ad62f1efd2af2900f308d87
Andrew Doctor <adocto@users.noreply.github.com>
2020-02-29 15:57:34 -0500

    Create Solution.java

---
+++ HackerRank0/src/isPalindrome/Solution.java
@@ -0,0 +1,25 @@
+package isPalindrome;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        int left = 0;
+        int right = A.length() - 1;
+        System.out.println(right);
+
+
+
+
+    }
+}
+
+
+
+
