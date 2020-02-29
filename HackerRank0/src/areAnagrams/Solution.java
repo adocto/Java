123b558b82b99d4d7c0ccbb28c516fb95efa1fba
Andrew Doctor <adocto@users.noreply.github.com>
2020-02-29 16:20:41 -0500

    Create Solution.java

---
+++ HackerRank0/src/areAnagrams/Solution.java
@@ -0,0 +1,25 @@
+package areAnagrams;
+
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class Solution {
+
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        Map <Character,Integer> tableA = new Hashtable<Object, Object>();
+        Map <Character,Integer> tableB = new Hashtable<Object, Object>();
+
+        return ret;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
