87668ece427fa2d0cc29e56b1f954bbb29cbc9d9
Andrew Doctor <adocto@users.noreply.github.com>
2020-02-29 16:02:16 -0500

    Complete isPalindrome

--- HackerRank0/src/isPalindrome/Solution.java
+++ HackerRank0/src/isPalindrome/Solution.java
@@ -6,14 +6,24 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-
+        String isPalindrome = "Yes";
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
         /* Enter your code here. Print output to STDOUT. */
         int left = 0;
         int right = A.length() - 1;
-        System.out.println(right);
-
+        while(left < right){
+            if(A.charAt(left) == A.charAt(right)){
+                isPalindrome = "Yes";
+            }
+            else {
+                isPalindrome = "No";
+                break;
+            }
+            left++;
+            right--;
+        }
+        System.out.println(isPalindrome);
 
 
 
