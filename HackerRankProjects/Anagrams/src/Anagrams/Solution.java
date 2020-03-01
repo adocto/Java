1a0f32e3b1a48523a62a10bac26ad0e6c3a2a4f3
Andrew Doctor <adocto@users.noreply.github.com>
2020-03-01 15:34:30 -0500

    Complete anagram
    Completed anagram with array algorithm since this example does not want you to import hashmap.

--- HackerRankProjects/Anagrams/src/Anagrams/Solution.java
+++ HackerRankProjects/Anagrams/src/Anagrams/Solution.java
@@ -5,13 +5,34 @@ import java.util.Scanner;
 public class Solution {
 
     static boolean isAnagram(String a, String b) {
+        if(a.length() != b.length()){
+            return false;
+        }
         // Complete the function
-        int[] countStore  = new int[26];
+        Integer[] countStore  = new Integer[26];
+
         for(int i = 0; i < a.length(); i++){
-            
+            int current = (int) Character.toUpperCase(a.charAt(i));
+
+            if(countStore[current - 65] == null){
+                countStore[current-65] = 1;
+            }else{
+                countStore[current-65] = countStore[current-65] + 1;
+            }
         }
 
+        for(int j = 0; j < b.length(); j++){
+            int current = (int) Character.toUpperCase(b.charAt(j));
+            if(countStore[current-65]== null){
+                return false;
+            }else if(countStore[current-65] == 0){
+                return false;
+            }else{
+                countStore[current-65] = countStore[current-65] - 1;
+            }
 
+        }
+        return true;
     }
 
     public static void main(String[] args) {
