166ef16e0dcb7e82c0a58bf549598196f55d6aac
Andrew Doctor <adocto@users.noreply.github.com>
2020-03-01 15:17:58 -0500

    Start Anagram
    Started Anagram exercise on hackerrank

--- HackerRankProjects/Anagrams/src/Anagrams/Solution.java
+++ HackerRankProjects/Anagrams/src/Anagrams/Solution.java
@@ -6,6 +6,12 @@ public class Solution {
 
     static boolean isAnagram(String a, String b) {
         // Complete the function
+        int[] countStore  = new int[26];
+        for(int i = 0; i < a.length(); i++){
+            
+        }
+
+
     }
 
     public static void main(String[] args) {
