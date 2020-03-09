c9b59118ebb6e1f67fb22686dbc59c7aba3a16c4
Andrew Doctor <adocto@users.noreply.github.com>
2020-03-08 20:28:38 -0400

    Update converToBinary

--- convertToBinary/src/convertToBinary/Solution.java
+++ convertToBinary/src/convertToBinary/Solution.java
@@ -17,9 +17,20 @@ public class Solution {
     public static void main(String[] args) {
         //stack for storing binary members
         Stack<int> binStack = new Stack<int>();
+        //The binary member to add to stack
+        int result;
+
+
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         scanner.close();
+        //Get the remainder and add it to member stock.
+        while(n > 0){
+            result = n%0;
+            n = n/2;
+            binStack.add(result);
+        }
+
     }
 }
