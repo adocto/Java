3db6ffe991769fe8a7b1d4f923a2e5e1f12230a3
Andrew Doctor <adocto@users.noreply.github.com>
2020-03-10 10:42:31 -0400

    Update convertToBinary

--- convertToBinary/src/convertToBinary/Solution.java
+++ convertToBinary/src/convertToBinary/Solution.java
@@ -21,5 +21,27 @@ public class Solution {
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         scanner.close();
+        int binMem;
+
+        while(n>0){
+            binMem = n%2;
+            binStack.add(binMem);
+            n = n/2;
+        }
+        int currentMax = 0;
+        int max1 = 0;
+
+        while(binStack.size() > 0){
+            if(binStack.pop() == 1){
+                currentMax++;
+                if(currentMax > max1) {
+                    max1 = currentMax;
+                }
+            }else{
+                currentMax = 0;
+            }
+        }
+
+
     }
 }
