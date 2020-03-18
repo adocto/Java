9404fc563d90c6818a9bc0c89574314a1e4c7c4c
Andrew Doctor <adocto@users.noreply.github.com>
2020-03-17 20:55:17 -0400

    Update speedbynumbers
    Test case allowed for negative speeds

--- HackerRankProjects/SkatebyNumbers/src/Solution.java
+++ HackerRankProjects/SkatebyNumbers/src/Solution.java
@@ -8,17 +8,21 @@ public class Main {
         int N = myKeyboard.nextInt();
         int[] height = new int[N];
         int speed = 1;
+        int max = 1;
         for(int i = 0; i < N; i++){
             height[i] = myKeyboard.nextInt();
         }
         for(int j = 1; j < N; j++){
-            if(height[j] > height[j-1]){
+            if((height[j] > height[j-1])){//&& speed > 0
                 speed = speed - 1;
-            }else{
+            }else if (height[j] < height[j-1]){
                 speed = speed+1;
             }
-            System.out.println(speed);
+            if(speed > max){
+                max = speed;
+            }
+
         }
-        System.out.println(speed);
+        System.out.println(max);
     }
 }
